f3c59d2311d30004c0461d136d0ff4707f3cfc56
ojo-perezoso <bymartin24@gmail.com>
2021-11-26 08:37:47 -0800

    Added _gentcommand() shell_test.c

--- day_0/shell_test.c
+++ day_0/shell_test.c
@@ -1,12 +1,74 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <dirent.h>
+/**
+ * _getcommand - gets the line and separates into tokens
+ * @token_list: pointer to token list
+ *
+ * Return: 0 on succes | -1 on failure
+ */
+int _getcommand(char *token_list[])
+{
+	char *token, *line[1024];
+	int pos_tok = 0;
+	size_t n;
+	n = 1024;
+
+	printf("> ");
+	getline(line, &n, stdin);
+
+	token = strtok(*line, " ");
+	while (token != NULL)
+	{
+		printf("t: %s, ", token);
+		token_list[pos_tok] = token;
+		pos_tok++;
+		token = strtok(NULL, " ");
+	}
+	return (0);
+}
+
+/**
+ * _getenv - gets path dirs and puts it into array
+ * @path_list: array to be stored at
+ * @envp: array of enviromental variables
+ * Return: 0 if success
+ */
+int _getenv(char *path_list[], char *envp[])
+{
+	int i = 0, pos_path = 0;
+	char *token;
+
+	for (i = 0; envp[i]; i++)
+	{
+		token = strtok(envp[i], "=");
+		if (strcmp(token, "PATH") == 0)
+		{
+			token = strtok(NULL, "=");
+			token = strtok(token, ":");
+			printf("token is: %s\n", token);
+			while (token != NULL)
+			{
+				path_list[pos_path] = token;
+				pos_path++;
+				token = strtok(NULL, ":");
+			}
+		}
+
+	}
+	if (path_list == NULL)
+		return (-1);
+	return (0);
+}
 
 int main(int argc, char *argv[], char *envp[])
 {
-	char *buff[1024], *token, *path_list[1024], *token_list[1024];
+	char *buff[1024], *token, *path_list[1024] = { NULL }, *token_list[1024];
 	size_t buffsize;
-	int pos_tok = 0, pos_env, pos_path;
+	int pos_tok = 0, pos_path;
+	DIR *dr;
+	struct dirent *de;
 
 	buffsize = 1024, *path_list = NULL, *token_list = NULL;
 	while (1)
@@ -24,47 +86,17 @@ int main(int argc, char *argv[], char *envp[])
 		printf("list of tokens\n");
 		for (pos_tok = 0; token_list[pos_tok]; pos_tok++)
 			printf("token is: %s\n", token_list[pos_tok]);
-		for (pos_env = 0; envp[pos_env]; pos_env++)
-		{
-			token = strtok(envp[pos_env], "=");
-			if (strcmp(token, "PATH") == 0)
-			{
-				token = strtok(NULL, "=");
-				token = strtok(token, ":");
-				while (token != NULL)
-				{
-					path_list[pos_path] = token;
-					pos_path++;
-					token = strtok(NULL, ":");
-				}
-			}
-		}
-		printf("list of path dirs\n");
+
+		_getenv(path_list, envp);
 		for (pos_path = 0; path_list[pos_path]; pos_path++)
-			printf("path token is: %s\n", path_list[pos_path]);
+		{
+			dr = opendir(path_list[pos_path]);
+			if (dr == NULL)
+				return (-1);
+			while ((de = readdir(dr)) != NULL)
+				printf("%s\n", de->d_name);
+		}
 	}
 	return (0);
-}
 
-char *_getenv(char *envp[])
-{
-	int pos_env, pos_path;
-	char *token, *path_list[1024];
-
-	for (pos_env = 0; envp[pos_env]; pos_env++)
-	{
-		token = strtok(envp[pos_env], "=");
-		if (strcmp(token, "PATH") == 0)
-		{
-			token = strtok(NULL, "=");
-			token = strtok(token, ":");
-			while (token != NULL)
-			{
-				path_list[pos_path] = token;
-				pos_path++;
-				token = strtok(NULL, ":");
-			}
-		}
-	}
-	return (*path_list);
 }
