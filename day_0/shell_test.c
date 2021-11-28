3c382b346017ab96eba94afaf3d37e81aedcafd6
ojo-perezoso <bymartin24@gmail.com>
2021-11-28 05:27:14 -0800

    modify shell_test1.c

--- day_0/shell_test.c
+++ day_0/shell_test.c
@@ -8,24 +8,19 @@
  *
  * Return: 0 on succes | -1 on failure
  */
-int _getcommand(char *token_list[])
+int _getcommand(char *token_list[], char *line[])
 {
-	char *token, *line[1024];
+	char *token = NULL;
 	int pos_tok = 0;
-	size_t n;
-	n = 1024;
-
-	printf("> ");
-	getline(line, &n, stdin);
 
 	token = strtok(*line, " ");
 	while (token != NULL)
 	{
-		printf("t: %s, ", token);
 		token_list[pos_tok] = token;
 		pos_tok++;
 		token = strtok(NULL, " ");
 	}
+	free(token);
 	return (0);
 }
 
@@ -35,21 +30,23 @@ int _getcommand(char *token_list[])
  * @envp: array of enviromental variables
  * Return: 0 if success
  */
+
 int _getenv(char *path_list[], char *envp[])
 {
-	int i = 0, pos_path = 0;
 	char *token;
+	int i = 0, pos_path = 0;
+	size_t l = 4;
 
+	token = NULL;
 	for (i = 0; envp[i]; i++)
 	{
 		token = strtok(envp[i], "=");
-		if (strcmp(token, "PATH") == 0)
+		if (strncmp(token, "PATH", l) == 0)
 		{
 			token = strtok(NULL, "=");
 			token = strtok(token, ":");
 			while (token != NULL)
 			{
-				printf("token is: %s\n", token);
 				path_list[pos_path] = token;
 				pos_path++;
 				token = strtok(NULL, ":");
@@ -62,34 +59,66 @@ int _getenv(char *path_list[], char *envp[])
 	return (0);
 }
 
+
+int _findcommand(char *path_list[], char *command)
+{
+	char path[1024];
+	int pos_path = 0;
+	for (pos_path = 0; path_list[pos_path]; pos_path++)
+	{
+		strcpy(path, path_list[pos_path]);
+		strcat(path, "/");
+		strcat(path, command);
+		printf("concatenated path: %s", path);
+		/**
+		 * Cant succesfully modify path_list with path value
+		 *
+		 * strcpy(path_list[pos_path], path);
+		 */
+	}
+
+	return (0);
+}
+
+
+
+
 int main(int argc, char *argv[], char *envp[])
 {
-	char *buff[1024], *token, *path_list[1024] = { NULL }, *token_list[1024];
-	size_t buffsize;
-	int pos_tok = 0, pos_path;
-	DIR *dr;
-	struct dirent *de;
+	char *path_list[1024], *token_list[1024], *line[1024]/*, *s_path*/;
+	int i; /*pos_path = 0;*/
+	size_t n;
 
-	buffsize = 1024, *path_list = NULL, *token_list = NULL;
+	n = 1024;
+	(void)argc, (void)argv, (void)envp;
+	*line = NULL, *token_list = NULL, *path_list = NULL/*, s_path = NULL*/;
+
 	while (1)
 	{
-		_getcommand(token_list);
-		printf("list of tokens\n");
-		for (pos_tok = 0; token_list[pos_tok]; pos_tok++)
-			printf("token is: %s\n", token_list[pos_tok]);
+		printf("> ");
+		if ((getline(line, &n, stdin)) >= 0)
+			_getcommand(token_list, line);
 
 		_getenv(path_list, envp);
-		for (pos_path = 0; path_list[pos_path]; pos_path++)
-		{
-			dr = opendir(path_list[pos_path]);
-			if (dr == NULL)
-				return (-1);
-			while ((de = readdir(dr)) != NULL)
-				printf("token_list[0]: %s\n", token_list[0]);
-				if (strcmp(de->d_name, "ls") == 0)
-					printf("encontre: %s\n", de->d_name);
-		}
+
+		_findcommand(&path_list, token_list[0]);
+
+		printf("\n****COMMAND TOKENS****\n\n");
+		for (i = 0; token_list[i]; i++)
+			printf("token[%d]: %s\n", i, token_list[i]);
+	
+		printf("\n****PATH DIRECTORIES****\n\n");
+		for (i = 0; path_list[i]; i++)
+			printf("path[%d]: %s\n", i, path_list[i]);
+		
+		
+		
+		/*free line*/
+		for (i = 0; line[i]; i++)
+			free(line[i]);
+
 	}
+
 	return (0);
 
 }
