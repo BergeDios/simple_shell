31501900b47291e82a1b2d0354f0a3992d6bb5b7
BergeDios <santiago.goyret@gmail.com>
2021-11-28 10:31:46 -0800

    shell3 add access and execve

--- day_0/shell_test3.c
+++ day_0/shell_test3.c
@@ -2,28 +2,28 @@
 #include <stdlib.h>
 #include <string.h>
 #include <dirent.h>
+#include <unistd.h>
+#include <sys/wait.h>
 /**
  * _getcommand - gets the line and separates into tokens
  * @token_list: pointer to token list
- *
+ * @line: pointer to prompt input
  * Return: 0 on succes | -1 on failure
  */
-char **_getcommand(char *line2)
+int _getcommand(char *token_list[], char *line)
 {
-	char *token = NULL, **token_list;
+	char *token = NULL;
 	int pos_tok = 0;
 
-	printf("line in getcommand is: %s\n", line2);
-	token_list = malloc(sizeof(char) * 100);
-	token = strtok(line2, " ");
+	token = strtok(line, " ");
 	while (token != NULL)
 	{
-		printf("token: %s\n", token);
 		token_list[pos_tok] = token;
 		pos_tok++;
 		token = strtok(NULL, " ");
 	}
-	return (token_list);
+	free(token);
+	return (0);
 }
 
 /**
@@ -32,16 +32,14 @@ char **_getcommand(char *line2)
  * @envp: array of enviromental variables
  * Return: 0 if success
  */
-/*
- * int _getenv(char *path_list[], char *envp[])
+
+int _getenv(char *path_list[], char *envp[])
 {
-	int i = 0, pos_path = 0;
 	char *token;
+	int i = 0, pos_path = 0;
 	size_t l = 4;
 
 	token = NULL;
-	printf("entre a getenv\n");
-
 	for (i = 0; envp[i]; i++)
 	{
 		token = strtok(envp[i], "=");
@@ -51,7 +49,6 @@ char **_getcommand(char *line2)
 			token = strtok(token, ":");
 			while (token != NULL)
 			{
-				printf("token is: %s\n", token);
 				path_list[pos_path] = token;
 				pos_path++;
 				token = strtok(NULL, ":");
@@ -62,47 +59,75 @@ char **_getcommand(char *line2)
 	if (path_list == NULL)
 		return (-1);
 	return (0);
-}*/
+}
+
+
+int _findcommand(char *path_list[], char *commandyarg[], char *envp[])
+{
+	char path[1024], *argv[1024];
+	int pos_path = 0, a;
+	pid_t id;
+
+	a = 0, id = 0, *argv = NULL;
+	for (pos_path = 0; commandyarg[pos_path]; pos_path++)
+		argv[pos_path] = commandyarg[pos_path];
+	for (pos_path = 0; path_list[pos_path]; pos_path++)
+	{
+		strcpy(path, path_list[pos_path]);
+		strcat(path, "/");
+		strcat(path, commandyarg[0]);
+		a = access(path, (R_OK | X_OK));
+		if (a == 0)
+		{
+			id = fork();
+			if (id != 0)
+				wait(0);
+			else
+			{
+				printf("hello from the child process\n");
+				execve(path, argv, envp);
+			}
+		}
+		/**
+		 * Cant succesfully modify path_list with path value
+		 *
+		 * strcpy(path_list[pos_path], path);
+		 */
+	}
+
+	return (0);
+}
+
+
+
 
 int main(int argc, char *argv[], char *envp[])
 {
-	char *path_list[1024], **token_list, *line[1024], line2[1024]/*, *s_path*/;
-	/*int pos_path = 0;*/
+	char *path_list[1024], *token_list[1024], *line/*, *s_path*/;
 	size_t n;
-	int i;
 
 	n = 1024;
-	(void)argc, (void)argv, (void)envp;
-	*path_list = NULL/*, s_path = NULL*/;
+	(void)argc, (void)argv;
+	line = NULL, *token_list = NULL, *path_list = NULL/*, s_path = NULL*/;
 
 	while (1)
 	{
 		printf("> ");
-		getline(line, &n, stdin);
-		printf("line is: %s\n", *line);
-
-		while (*line[i] != '\n')
+		if ((getline(&line, &n, stdin)) >= 0)
 		{
-			printf("%c\n", *line[i]);
-			i++;
+			line[strlen(line) - 1] = '\0';
+			_getcommand(token_list, line);
 		}
-		printf("line2 is: %s\n", line2);
 
-		token_list = _getcommand(line2);
-		for (i = 0; token_list[i]; i++)
-			printf("tl[%d]: %s\n", i, token_list[i]);
+		_getenv(path_list, envp);
 
-		/*_getenv(path_list, envp);
-		for (pos_path = 0; path_list[pos_path]; pos_path++)
-			printf("%s\n", path_list[pos_path]);
-		for (pos_path = 0; path_list[pos_path]; pos_path++)
-		{
-			s_path = strcat(path_list[pos_path], "/");
-			s_path = strcat(s_path, token_list[0]);
-			printf("%s\n", s_path);
+		_findcommand(path_list, token_list, envp);
 
-		}*/
+		/*free line*/
+		free(line);
+
 	}
+
 	return (0);
 
 }
