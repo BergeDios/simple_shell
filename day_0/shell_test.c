0976015fbdf3f1fa40fe232922e72bb76c29a0db
ojo-perezoso <bymartin24@gmail.com>
2021-11-28 17:58:13 -0800

    Added exit for shell

--- day_0/shell_test.c
+++ day_0/shell_test.c
@@ -60,9 +60,10 @@ int _getenv(char *path_list[], char *envp[])
 int _findcommand(char *path_list[], char *token_list[], char *envp[])
 {
 	char path[1024];
-	int pos_path = 0, a = 0;
-	pid_t id = 0;
+	int pos_path = 0, a;
+	pid_t id;
 
+	a = 0, id = 0;
 	for (pos_path = 0; path_list[pos_path]; pos_path++)
 	{
 		strcpy(path, path_list[pos_path]);
@@ -71,25 +72,31 @@ int _findcommand(char *path_list[], char *token_list[], char *envp[])
 		a = access(path, (R_OK | X_OK));
 		if (a == 0)
 		{
+			printf("access granted from pid: %d\n", getpid());
+			printf("entered for of findcommand\n");
+
 			id = fork();
 			if (id != 0)
-				wait(0);
+				wait(NULL);
 			else
 			{
-				printf("hello from the child process\n");
+				printf("executing from pid: %d\n", getpid());
 				execve(path, token_list, envp);
+				return (0);
 			}
 		}
 	}
-
-	return (0);
+	return (-1);
 }
+
+
 
 
 int main(int argc, char *argv[], char *envp[])
 {
 	char *path_list[1024], *token_list[1024], *line/*, *s_path*/;
 	size_t n;
+	int i;
 
 	n = 1024;
 	(void)argc, (void)argv;
@@ -101,16 +108,32 @@ int main(int argc, char *argv[], char *envp[])
 		if ((getline(&line, &n, stdin)) >= 0)
 		{
 			line[strlen(line) - 1] = '\0';
+			if (strncmp(line, "exit", 4) == 0)
+			{
+				printf("exiting\n");
+				return (0);
+			}
 			_getcommand(token_list, line);
 		}
 
 		_getenv(path_list, envp);
 
 		_findcommand(path_list, token_list, envp);
-
-
 	}
+	
+	for (i = 0; token_list[i]; i++)
+		free(token_list[i]);
+	free(token_list[i]);
+
+
+	for (i = 0; path_list[i]; i++)
+		free(path_list[i]);
+	free(path_list[i]);
+	free(path_list);
+	free(token_list);
+
 	free(line);
+
 	return (0);
 
 }
--- day_0/utility_funcs.c
+++ day_0/utility_funcs.c
@@ -56,9 +56,8 @@ int _strlen(char *s)
 	int j = 0;
 
 	while (s[j] != 0)
-	{
 		j++;
-	}
+
 	return (j);
 }
 /**
